b05ddce8bac7ab5af0917c0341a08912a4c5a83d
Sheplu <jean.burellier@gmail.com>
2017-03-14 13:26:20 +0100

    add list adapter

--- app/src/main/java/com/oromys/sheplu/supquotes/MainActivity.java
+++ app/src/main/java/com/oromys/sheplu/supquotes/MainActivity.java
@@ -2,10 +2,10 @@ package com.oromys.sheplu.supquotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.oromys.sheplu.supquotes.models.Quote;
@@ -20,14 +20,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textview = (TextView) findViewById(R.id.hello);
-        Log.i("Datas", "onCreate: ID" + textview.getText().toString());
 
         String[] quotes = getResources().getStringArray(R.array.quotes);
         for (String item: quotes) {
             Log.i("Datas", "onCreate: "+ item);
             addQuote(item);
         }
+
         showQuotes(arrayListQuote);
 
     }
@@ -38,11 +37,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void showQuotes(ArrayList<Quote> quotes) {
+
+        ListView lv = (ListView) findViewById(R.id.lv);
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, getAllQuotes(quotes));
+        lv.setAdapter(listAdapter);
         for (Quote quote : quotes) {
             Log.i("Datas", "showQuotes: "+quote.getStrQuote());
             Toast toast = Toast.makeText(getApplicationContext(),
                     quote.getStrQuote(), Toast.LENGTH_LONG);
             toast.show();
         }
+    }
+
+    protected ArrayList<String> getAllQuotes(ArrayList<Quote> quotes) {
+        ArrayList<String> listQuotes = new ArrayList<>();
+        for (Quote quote: quotes) {
+            listQuotes.add(quote.getStrQuote());
+        }
+        return listQuotes;
     }
 }
