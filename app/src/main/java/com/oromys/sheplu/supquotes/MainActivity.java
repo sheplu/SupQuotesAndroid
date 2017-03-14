704940af00c183c14efae9554c1df29669b086ca
Sheplu <jean.burellier@gmail.com>
2017-03-14 14:47:22 +0100

    add quote from input

--- app/src/main/java/com/oromys/sheplu/supquotes/MainActivity.java
+++ app/src/main/java/com/oromys/sheplu/supquotes/MainActivity.java
@@ -2,18 +2,23 @@ package com.oromys.sheplu.supquotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.oromys.sheplu.supquotes.models.Quote;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button addButton;
+    private String[] quotes;
+    private Quote newQuote;
+    private  ArrayAdapter<String> listAdapter;
+    private ArrayList<String> listQuotes;
     ArrayList<Quote> arrayListQuote = new ArrayList<>();
 
     @Override
@@ -21,40 +26,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] quotes = getResources().getStringArray(R.array.quotes);
-        for (String item: quotes) {
-            Log.i("Datas", "onCreate: "+ item);
-            addQuote(item);
-        }
+        addButton = (Button) findViewById(R.id.addButton);
+        addButton.setOnClickListener(this);
 
+        loadQuotes();
         showQuotes(arrayListQuote);
 
     }
 
+    protected void loadQuotes() {
+        quotes = getResources().getStringArray(R.array.quotes);
+        for (String item: quotes) {
+            addQuote(item);
+        }
+    }
     protected void addQuote(String quote) {
-        Quote newQuote = new Quote(quote);
+        newQuote = new Quote(quote);
         arrayListQuote.add(newQuote);
     }
 
     protected void showQuotes(ArrayList<Quote> quotes) {
-
         ListView lv = (ListView) findViewById(R.id.lv);
-        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,
+        listAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, getAllQuotes(quotes));
         lv.setAdapter(listAdapter);
-        for (Quote quote : quotes) {
-            Log.i("Datas", "showQuotes: "+quote.getStrQuote());
-            Toast toast = Toast.makeText(getApplicationContext(),
-                    quote.getStrQuote(), Toast.LENGTH_LONG);
-            toast.show();
-        }
     }
 
     protected ArrayList<String> getAllQuotes(ArrayList<Quote> quotes) {
-        ArrayList<String> listQuotes = new ArrayList<>();
+        listQuotes = new ArrayList<>();
         for (Quote quote: quotes) {
             listQuotes.add(quote.getStrQuote());
         }
         return listQuotes;
+    }
+
+    public void onClick(View view) {
+        InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        EditText edittext =  (EditText) findViewById(R.id.addQuote);
+        String newQuoteString = edittext.getText().toString();
+        addQuote(newQuoteString);
+        showQuotes(arrayListQuote);
+        edittext.setText("");
     }
 }
